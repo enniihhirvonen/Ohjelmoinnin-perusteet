937014828ef4bbeab4c7ef880da928110c294a23
Enni Hirvonen <enniih.hirvonen@gmail.com>
2025-09-14 18:06:51 +0300

    sixth commit

---
+++ week-1/A1_T7.py
@@ -0,0 +1,8 @@
+print("Calculate fuel consumption.")
+Feed=input("Enter travel distance (kilometers): ")
+Distance=int(Feed)
+Feed=input("Enter fuel usage (liters): ")
+FuelUsage=int(Feed)
+Consumption=(Distance / FuelUsage)
+Consumption=int(Consumption)
+print("Fuel consumption is", Consumption, "l per 100 km.")
