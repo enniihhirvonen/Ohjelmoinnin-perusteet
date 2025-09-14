8984d89e35e42793a932c7400818c5c9af4e4267
Enni Hirvonen <enniih.hirvonen@gmail.com>
2025-09-14 18:02:03 +0300

    fifth commit

--- week-1/A1_T2.py
+++ week-1/A1_T2.py
@@ -2,6 +2,6 @@ name1="John"
 print(name1)
 name2="Harry"
 print(name2)
-print(name1," ",name2)
+print(name1, name2)
 print(name1, " is eating ice cream with ", name2)
 print(name1+" and "+name2+" are friends")
--- week-1/A1_T4.py
+++ week-1/A1_T4.py
@@ -1,8 +1,8 @@
 Num1=47
 Num2=102
 Sum=Num1+Num2
-Diff=(Num2-Num1)
-Product=(Sum*Diff)
+Diff=Num2-Num1
+Product=Sum*Diff
 print(Num1, "+", Num2, "=", Sum)
 print(Num2, "-", Num2, "=", Diff)
 print(Sum, "*", Diff, "=", Product)
